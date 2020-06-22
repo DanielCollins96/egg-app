8f81ca4bcf895f8e5e6b1210cb1d9d1a8140311e
dc <danielmcollins1996@gmail.com>
2020-06-21 20:38:27 -0600

    chart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Route
 } from "react-router-dom";
 import { Button, Alert } from 'reactstrap';
+import { Doughnut } from 'react-chartjs-2';
 
 import Navbar from './components/navbar';
 import './App.css';
@@ -38,34 +39,69 @@ export default function App() {
 }
 
 const Home = () => {
-  const [count, setCount] = useState(0)
+  const [yolks, setYolks] = useState(0)
+  const [whites, setWhites] = useState(0)
   const [visible, setVisibility] = useState(false)
 
   const increment = () => {
     setVisibility(false)
-    setCount(count + 1)
+    // setCount(count + 1)
   }
 
   const decrement = () => {
-    if (count > 0) 
-      return setCount(count - 1)
+    // if (count > 0) 
+    //   return setCount(count - 1)
     setVisibility(true)
   }
 
   const reset = () => {
     setVisibility(false)
-    setCount(0)
+    // setCount(0)
   }
+  const data = {
+    labels: [
+      'White',
+      'Yellow'
+    ],
+    datasets: [{
+      data: [whites, yolks],
+      backgroundColor: [
+      '#FFFF',
+      '#faf01b',
+      ],
+      hoverBackgroundColor: [
+        '#F8F8FF',
+      '#BFBF3F'
+      ]
+    }]
+  };
   return (
-    <div class="count">
+    <div className="count">
 
-    <h2>Home</h2>
-    <div>
-      <h3>Current Count: {count}</h3>
-      <Button onClick={increment}>Increment</Button>
-      <Button onClick={decrement}>Decrement</Button>
-      <Button onClick={() => {reset()}}>Reset</Button>
+    <h2>Eggs</h2>
+    <div className="option">
+      <h3>Egg Whites</h3>
+      <div className="inner__counter">
+      <Button onClick={() => {setWhites(whites - 1)}}>-</Button>
+      <h3>{whites}</h3>
+      <Button onClick={() => {setWhites(whites + 1)}}>+</Button>
+      </div>
+      <Button onClick={() => {setWhites(0)}}>Reset</Button>
+    </div>
+    <div className="option">
+      <h3>Egg Yolks</h3>
+      <div className="inner__counter">
+      {/* <Button onClick={decrement}>-</Button> */}
+      <Button onClick={() => {setYolks(yolks -1)}}>-</Button>
+      <h3>{yolks}</h3>
+      <Button onClick={() => {setYolks(yolks + 1)}}>+</Button>
+      </div>
+      <Button onClick={() => {setYolks(0)}}>Reset</Button>
+    </div>
       <Alert style={{width: '40%', margin: '10px auto'}} color="warning" isOpen={visible}>Can't be less than 0!</Alert>
+    <hr></hr>
+    <div className="chart">
+      <Doughnut data={data} />
     </div>
     </div>
   );
